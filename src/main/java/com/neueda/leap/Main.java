2d161c588d070db1159f0f6b400d299cec799726
Soli Ateefa <sateefa2904@gmail.com>
2026-08-24 18:32:13 +0000

    feat: add greeting method to Main

--- src/main/java/com/neueda/leap/Main.java
+++ src/main/java/com/neueda/leap/Main.java
@@ -3,5 +3,10 @@ package com.neueda.leap;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world from the team's Sprint 1 project skeleton");
+        System.out.println(greet("Sprint 2"));
+    }
+
+    public static String greet(String name) {
+        return "Hello, " + name + "!";
     }
 }
